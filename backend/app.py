cb11a6785ab238163fef30e30c39bae3903a2633
az-zieee <astro2638@gmail.com>
2026-05-16 14:51:53 +0800

    Merge

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-<<<<<<< HEAD
-from backend.Endpoints import adminAuth, service, submit_res
+from backend.Endpoints import adminAuth, service, submit_res, post_res, get_res, service, admin_sum
 
 app = FastAPI()
 
@@ -8,13 +7,6 @@ app = FastAPI()
 app.include_router(service.router)
 app.include_router(submit_res.router)
 app.include_router(adminAuth.router)
-=======
-from backend.Endpoints import get_service, post_res, admin_sum, get_res
-
-app = FastAPI()
-
-app.include_router(get_service.router)
 app.include_router(post_res.router)
 app.include_router(admin_sum.router)
 app.include_router(get_res.router)
->>>>>>> origin/backend/services
