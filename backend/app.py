44e8d25ed84f7cb6584a6c18a4ac9ac413f68f89
az-zieee <astro2638@gmail.com>
2026-05-16 15:09:07 +0800

    merge

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from backend.Endpoints import adminAuth, service, submit_res, post_res, get_res, service, admin_sum
+from backend.Endpoints import adminAuth, admin_sum, post_res, get_res, get_service
 
 app = FastAPI()
 
 # Include routers from each file
-app.include_router(service.router)
-app.include_router(submit_res.router)
 app.include_router(adminAuth.router)
-app.include_router(post_res.router)
 app.include_router(admin_sum.router)
+app.include_router(post_res.router)
 app.include_router(get_res.router)
+app.include_router(get_service.router)
